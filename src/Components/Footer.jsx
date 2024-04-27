955fdaf5017f3de931a7b885e7d0e4b530ac489d
Hasan Rahic <haso.raha10@gmail.com>
2024-04-27 13:34:57 +0200

    making navbar and footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Football app 2021&copy;</p>
+    </div>
+  )
+}
+
+export default Footer
