d1c745c109ac7fce6f8c7d9a12d44f675e76ecb1
Hasan Rahic <haso.raha10@gmail.com>
2024-05-01 18:42:16 +0200

    fetching api

--- src/Components/Leagues.jsx
+++ src/Components/Leagues.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import '../App.css';
 
 const Leagues = () => {
+
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    axios("https://api-football-standings.azharimm.site/leagues").then(
+      (res)=>{
+        console.log(res.data.data);
+        setData(res.data.data);
+    }
+  );
+  }, []);
+
   return (
     <div className='leagues-container'>
-      Leagues
+      {data.map((data)=> (
+        <div key={data.id} className="league-div">
+          <img src={data.logos.dark} alt='#'/>
+          <h1>{data.name}</h1>
+        </div>
+      ))}
     </div>
   )
 }
